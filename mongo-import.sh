2ce6bf85568b13ce3999fd53098deef0faa3d258
Ferdinand Prantl <prantlf@gmail.com>
2014-07-27 12:11:25 +0200

    Capitalize colection names to match the schema at odata.org

--- mongo-import.sh
+++ mongo-import.sh
@@ -3,5 +3,7 @@ do
     filename=$(basename "$f")
     extension="${filename##*.}"
     filename="${filename%.*}"
+    filename=`echo "$filename" | sed 's/[^-]*/\u&/g'`
+    filename=`echo "$filename" | sed 's/-//g'`
     mongoimport -d Northwind -c "$filename" --type csv --file "$f" --headerline
 done
